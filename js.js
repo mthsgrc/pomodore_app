469b2c2aecac020cac0db930737187f201cb3c50
mthsgrc <matheus.dev.garcia@gmail.com>
2020-03-17 11:26:55 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+/* eslint-env es6 */
+
+const playBtn = document.querySelector(".play-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const pomodoreDisplay = document.querySelector(".pomodore-display");
+
+
+
