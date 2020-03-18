d21d2329951040a834cdb26e4eb126f2b6417abe
mthsgrc <matheus.dev.garcia@gmail.com>
2020-03-18 14:31:01 -0300

    pomodore working. Adjust some style

--- js.js
+++ js.js
@@ -2,7 +2,105 @@
 
 const playBtn = document.querySelector(".play-btn");
 const resetBtn = document.querySelector(".reset-btn");
-const pomodoreDisplay = document.querySelector(".pomodore-display");
+// const displayMinutes = document.querySelector("#display-minutes");
+// const displaySeconds = document.querySelector("#display-seconds");
+// const pomodoreDisplay = document.querySelector(".pomodore-display");
+
+//create pomodore obj
+let pomodore = {};
+
+
+//run initial pomodore set up
+initialPomodore();
 
 
 
+//set up values for pomodore, and insert into DOM
+function initialPomodore() {
+
+    // values for pomodore work and rest
+    pomodore.totalSeconds = 1500;
+    pomodore.restSeconds = 300;
+
+    // get the display of valuew (minutes and seconds)
+    pomodore.minutes = document.getElementById("display-minutes");
+    pomodore.seconds = document.getElementById("display-seconds");
+
+    // calculate remaining time
+    let seconds = pomodore.totalSeconds;
+    let minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+
+    // update HTML
+    pomodore.minutes.innerHTML = ("0" + minutes).slice(-2);
+    pomodore.seconds.innerHTML = ("0" + seconds).slice(-2);
+
+}
+
+
+//click event listener to start pomodore timer
+playBtn.addEventListener("click", () => {
+    // Hides play button
+    playBtn.style.visibility = "hidden";
+
+    if (pomodore.totalSeconds > 0) {
+        pomodore.ticker = setInterval(() => {
+
+            //stop if end time has passed
+            pomodore.totalSeconds--;
+            if (pomodore.totalSeconds <= 0) {
+                clearInterval(pomodore.ticker);
+                pomodore.totalSeconds = 0;
+                startRest();
+            }
+
+            // calculate remaining time
+
+            let seconds = pomodore.totalSeconds;
+            let minutes = Math.floor(seconds / 60);
+            seconds -= minutes * 60;
+
+            // update HTML
+            pomodore.minutes.innerHTML = ("0" + minutes).slice(-2);
+            pomodore.seconds.innerHTML = ("0" + seconds).slice(-2);
+
+        }, 1000);
+    }
+});
+
+
+function startRest() {
+    if (pomodore.restSeconds > 0) {
+        pomodore.ticker = setInterval(() => {
+
+            //stop if end time has passed
+            pomodore.restSeconds--;
+            if (pomodore.restSeconds <= 0) {
+                clearInterval(pomodore.ticker);
+                pomodore.restSeconds = 0;
+            }
+
+            // calculate remaining time
+
+            let seconds = pomodore.restSeconds;
+            let minutes = Math.floor(seconds / 60);
+            seconds -= minutes * 60;
+
+            // update HTML
+            pomodore.minutes.innerHTML = ("0" + minutes).slice(-2);
+            pomodore.seconds.innerHTML = ("0" + seconds).slice(-2);
+
+        }, 1000);
+    }
+}
+
+// reset/stops pomodore
+resetBtn.addEventListener("click", () => {
+    //stop setInterval function on counter
+    clearInterval(pomodore.ticker);
+    //set up pomodore initial values
+    initialPomodore();
+    //start btn visible again
+    playBtn.style.visibility = "visible";
+
+});
